7d31381ad6739a7801bc0443ba0c4144d98b3ce6
Arin <arinpamukcu@gmail.com>
2022-08-30 15:41:43 -0500

    update

--- turn_trig_spike.py
+++ turn_trig_spike.py
@@ -34,8 +34,8 @@ def ctrl():
 
             # remove events in first 25 and last 25 frames
             window = 25
-            # mars_turn_ctrl_modified = mars_left_angle_ctrl[window:-window]
-            mars_turn_ctrl_modified = mars_right_angle_ctrl[window:-window]
+            mars_turn_ctrl_modified = mars_left_angle_ctrl[window:-window]
+            # mars_turn_ctrl_modified = mars_right_angle_ctrl[window:-window]
 
             # find left turn triggered Ca event dff average per neuron and per animal
             # find frames where angle gets smaller for five consecutive frames (i.e. 1 second)
@@ -104,8 +104,8 @@ def amph():
 
             # remove events in first 25 and last 25 frames
             window = 25
-            # mars_turn_amph_modified = mars_left_angle_amph[window:-window]
-            mars_turn_amph_modified = mars_right_angle_amph[window:-window]
+            mars_turn_amph_modified = mars_left_angle_amph[window:-window]
+            # mars_turn_amph_modified = mars_right_angle_amph[window:-window]
 
             # find left turn triggered Ca event dff average per neuron and per animal
             # find frames where angle gets smaller for five consecutive frames (i.e. 1 second)
@@ -181,7 +181,7 @@ def plot():
     plt.xlabel('Time (s) from turn bout')
     plt.ylabel('Ca event rate (event/min)')
     plt.title('CTRL')
-    plt.suptitle('Right turn triggered Ca activity')
+    plt.suptitle('Left turn triggered Ca activity')
     plt.legend()
 
     plt.subplot(212)
@@ -212,7 +212,7 @@ def plot():
     plt.xlabel('Time (s) from turn bout')
     plt.ylabel('Ca event rate (event/min)')
     plt.title('CTRL')
-    plt.suptitle('Right turn triggered Ca activity')
+    plt.suptitle('Left turn triggered Ca activity')
     plt.legend()
 
     plt.subplot(212)
