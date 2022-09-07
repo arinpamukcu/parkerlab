b47344192147b643df02a4a0c0450c6410d0724e
Arin <arinpamukcu@gmail.com>
2022-09-07 16:56:11 -0500

    update

--- dff/time_spent_speed.py
+++ dff/time_spent_speed.py
@@ -7,24 +7,6 @@ from mars import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-def turn:
-    # drugs = ['Clozapine']
-    drugs = get_drug()
-    dose = 'Vehicle'
-
-    for drug in drugs:
-
-        experiments, D1_folders, D2_folders = get_animal_id(drug, dose)
-
-        for experiment in experiments:
-            print(experiment + '_right')
-
-            speed_ctrl, speed_amph, _, _, _, _, \
-            _, _, _, _, _, _, eventmean_ctrl, eventmean_amph, neuron, time_ctrl, time_amph = get_data(drug, dose,
-                                                                                                      experiment)
-
-            _, _, _, _, mars_right_angle_ctrl, mars_right_angle_amph = mars_feature(drug, dose, experiment)
-
 def speed:
     # drugs = ['Clozapine']
     drugs = get_drug()
@@ -42,3 +24,5 @@ def speed:
                                                                                                       experiment)
 
             _, _, _, _, mars_right_angle_ctrl, mars_right_angle_amph = mars_feature(drug, dose, experiment)
+
+    return
---
+++ dff/time_spent_turn.py
@@ -0,0 +1,29 @@
+# Created by Arin Pamukcu, PhD on August 2022
+# histogras for time spent doing particular action
+
+from data import *
+from info import *
+from mars import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def turn:
+    # drugs = ['Clozapine']
+    drugs = get_drug()
+    dose = 'Vehicle'
+
+    for drug in drugs:
+
+        experiments, D1_folders, D2_folders = get_animal_id(drug, dose)
+
+        for experiment in experiments:
+            print(experiment + '_right')
+
+            speed_ctrl, speed_amph, _, _, _, _, \
+            _, _, _, _, _, _, eventmean_ctrl, eventmean_amph, neuron, time_ctrl, time_amph = get_data(drug, dose,
+                                                                                                      experiment)
+
+            _, _, mars_left_angle_ctrl, mars_left_angle_amph, mars_right_angle_ctrl, mars_right_angle_amph \
+                = mars_feature(drug, dose, experiment)
+
+    return
