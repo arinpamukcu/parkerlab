1e5f9ecced225d434f4cc95d5a44694555f1c005
Arin Pamukcu <arinpamukcu@gmail.com>
2022-09-30 18:03:14 -0500

    update

--- event_turn_speed.py
+++ event_turn_speed.py
@@ -41,15 +41,16 @@ def speed_bins(speed_data, turn_data, speed, eventmean_data):
     return event_turn_speed
 
 def data_ctrl():
-    drugs = ['Clozapine', 'Haloperidol', 'MP-10', 'Olanzapine']
+    # drugs = ['Clozapine', 'Haloperidol', 'MP-10', 'Olanzapine']
+    drugs = ['Clozapine']
     dose = 'Vehicle'
+
+    D1_ets_ctrl = []
+    D2_ets_ctrl = []
 
     for drug in drugs:
 
         experiments, _, D1_folders, D2_folders = get_animal_id(drug, dose)
-
-        D1_ets_ctrl = {}
-        D2_ets_ctrl = {}
 
         for experiment in experiments:
             print(experiment)
@@ -57,36 +58,38 @@ def data_ctrl():
             speed_ctrl, speed_amph, _, _, eventmean_ctrl, eventmean_amph, _, _, _ = get_data(drug, dose, experiment)
             turn_ctrl, turn_amph, _, _ = mars_feature(drug, dose, experiment)
 
-            ets_ctrl_0 = speed_bins(speed_ctrl, turn_ctrl, 1, eventmean_ctrl)
-            ets_ctrl_30 = speed_bins(speed_ctrl, turn_ctrl, 1, eventmean_ctrl)
-            ets_ctrl_60 = speed_bins(speed_ctrl, turn_ctrl, 1, eventmean_ctrl)
+            ets_ctrl = speed_bins(speed_ctrl, turn_ctrl, 1, eventmean_ctrl)
 
             if experiment in D1_folders:
-                D1_ets_ctrl['0'] = ets_ctrl_0
-                D1_ets_ctrl['30'] = ets_ctrl_30
-                D1_ets_ctrl['60'] = ets_ctrl_60
+                D1_ets_ctrl.append(ets_ctrl)
 
             elif experiment in D2_folders:
-                D2_ets_ctrl['0'] = ets_ctrl_0
-                D2_ets_ctrl['30'] = ets_ctrl_30
-                D2_ets_ctrl['60'] = ets_ctrl_60
+                D2_ets_ctrl.append(ets_ctrl)
 
-    pickle.dump(D1_ets_ctrl, open("D1_ets_ctrl.pkl", "wb"))
-    pickle.dump(D2_ets_ctrl, open("D2_ets_ctrl.pkl", "wb"))
+    D1_ets_ctrl_mean = np.mean(D1_ets_ctrl)
+    D2_ets_ctrl_mean = np.mean(D2_ets_ctrl)
 
-    return D1_ets_ctrl, D2_ets_ctrl
+    D1_ets_ctrl_sem = np.std(D1_ets_ctrl) / np.sqrt(len(D1_ets_ctrl))
+    D2_ets_ctrl_sem = np.std(D2_ets_ctrl) / np.sqrt(len(D2_ets_ctrl))
+
+    pickle.dump(D1_ets_ctrl_mean, open("D1_ets_ctrl_mean.pkl", "wb"))
+    pickle.dump(D1_ets_ctrl_sem, open("D1_ets_ctrl_sem.pkl", "wb"))
+    pickle.dump(D2_ets_ctrl_mean, open("D2_ets_ctrl_mean.pkl", "wb"))
+    pickle.dump(D2_ets_ctrl_sem, open("D2_ets_ctrl_sem.pkl", "wb"))
+
+    return D1_ets_ctrl_mean, D1_ets_ctrl_sem, D2_ets_ctrl_mean, D2_ets_ctrl_sem
 
 
 def data_amph():
     drugs = ['Clozapine']
     dose = 'Vehicle'
 
+    D1_ets_amph = []
+    D2_ets_amph = []
+
     for drug in drugs:
 
         experiments, _, D1_folders, D2_folders = get_animal_id(drug, dose)
-
-        D1_ets_amph = {}
-        D2_ets_amph = {}
 
         for experiment in experiments:
             print(experiment + '_amph')
@@ -94,65 +97,69 @@ def data_amph():
             speed_ctrl, speed_amph, _, _, eventmean_ctrl, eventmean_amph, _, _, _ = get_data(drug, dose, experiment)
             turn_ctrl, turn_amph, _, _ = mars_feature(drug, dose, experiment)
 
-            ets_amph_0 = speed_bins(speed_amph, turn_amph, 1, eventmean_amph)
-            ets_amph_30 = speed_bins(speed_amph, turn_amph, 1, eventmean_amph)
-            ets_amph_60 = speed_bins(speed_amph, turn_amph, 1, eventmean_amph)
+            ets_amph = speed_bins(speed_amph, turn_amph, 1, eventmean_amph)
 
             if experiment in D1_folders:
-                D1_ets_amph['0'] = ets_amph_0
-                D1_ets_amph['30'] = ets_amph_30
-                D1_ets_amph['60'] = ets_amph_60
+                D1_ets_amph.append(ets_amph)
 
             elif experiment in D2_folders:
-                D2_ets_amph['0'] = ets_amph_0
-                D2_ets_amph['30'] = ets_amph_30
-                D2_ets_amph['60'] = ets_amph_60
+                D2_ets_amph.append(ets_amph)
 
-    pickle.dump(D1_ets_amph, open("D1_ets_amph.pkl", "wb"))
-    pickle.dump(D2_ets_amph, open("D2_ets_amph.pkl", "wb"))
+    D1_ets_amph_mean = np.mean(D1_ets_amph)
+    D2_ets_amph_mean = np.mean(D2_ets_amph)
 
-    return D1_ets_amph, D2_ets_amph
+    D1_ets_amph_sem = np.std(D1_ets_amph) / np.sqrt(len(D1_ets_amph))
+    D2_ets_amph_sem = np.std(D2_ets_amph) / np.sqrt(len(D2_ets_amph))
+
+    pickle.dump(D1_ets_amph_mean, open("D1_ets_amph_mean.pkl", "wb"))
+    pickle.dump(D1_ets_amph_sem, open("D1_ets_amph_sem.pkl", "wb"))
+    pickle.dump(D2_ets_amph_mean, open("D2_ets_amph_mean.pkl", "wb"))
+    pickle.dump(D2_ets_amph_sem, open("D2_ets_amph_sem.pkl", "wb"))
+
+    return D1_ets_amph_mean, D1_ets_amph_sem, D2_ets_amph_mean, D2_ets_amph_sem
 
 
 def plot():
 
     # D1_ets_ctrl, D2_ets_ctrl = data_ctrl()
-    D1_ets_ctrl = pickle.load(open("D1_ets_ctrl.pkl", "rb"))
-    D2_ets_ctrl = pickle.load(open("D2_ets_ctrl.pkl", "rb"))
+    D1_ets_ctrl_mean = pickle.load(open("D1_ets_ctrl_mean.pkl", "rb"))
+    D1_ets_ctrl_sem = pickle.load(open("D1_ets_ctrl_sem.pkl", "rb"))
+    D2_ets_ctrl_mean = pickle.load(open("D2_ets_ctrl_mean.pkl", "rb"))
+    D2_ets_ctrl_sem = pickle.load(open("D2_ets_ctrl_sem.pkl", "rb"))
     # D1_ets_amph, D2_ets_amph = data_amph()
-    D1_ets_amph = pickle.load(open("D1_ets_amph.pkl", "rb"))
-    D2_ets_amph = pickle.load(open("D2_ets_amph.pkl", "rb"))
+    D1_ets_amph_mean = pickle.load(open("D1_ets_amph_mean.pkl", "rb"))
+    D1_ets_amph_sem = pickle.load(open("D1_ets_amph_sem.pkl", "rb"))
+    D2_ets_amph_mean = pickle.load(open("D2_ets_amph_mean.pkl", "rb"))
+    D2_ets_amph_sem = pickle.load(open("D2_ets_amph_sem.pkl", "rb"))
+
+    x=range(5)
 
     plt.figure(figsize=(5, 9))
     plt.subplot(211)
-    plt.plot(D1_ets_ctrl['0'], label='D1 ctrl 0', color='k')
-    plt.plot(D1_ets_ctrl['30'], label='D1 ctrl 30', color='k', linestyle='--')
-    plt.plot(D1_ets_ctrl['60'], label='D1 ctrl 60', color='k', linestyle=':')
-    plt.plot(D1_ets_amph['0'], label='D1 amph 0', color='b')
-    plt.plot(D1_ets_amph['30'], label='D1 amph 30', color='b', linestyle='--')
-    plt.plot(D1_ets_amph['60'], label='D1 amph 60', color='b', linestyle=':')
-    x_default = [0, 1, 2, 3];
-    x_new = ['<1', '1-5', '5-10', '>10'];
+    plt.plot(D1_ets_ctrl_mean, label='D1 ctrl', color='k')
+    plt.fill_between(x, D1_ets_ctrl_mean + D1_ets_ctrl_sem, D1_ets_ctrl_mean - D1_ets_ctrl_sem, color='k', alpha=0.2)
+    plt.plot(D1_ets_amph_mean, label='D1 amph', color='b')
+    plt.fill_between(x, D1_ets_ctrl_mean + D1_ets_ctrl_sem, D1_ets_ctrl_mean - D1_ets_ctrl_sem, color='b', alpha=0.2)
+    x_default = [0, 1, 2, 3, 4];
+    x_new = ['right 60', 'right 30', 'straight 0', 'left 30', 'left 60'];
     plt.xticks(x_default, x_new);
     plt.ylim((0, 2.5))
-    plt.xlabel('Locomotor speed bin (cm/s)')
+    # plt.xlabel('Locomotor speed bin (cm/s)')
     plt.ylabel('Ca event rate (event/min)')
     plt.title('D1 SPNs')
-    plt.suptitle('Ca spike per speed bout')
+    plt.suptitle('Ca events for speed=1cm/s')
     plt.legend()
 
     plt.subplot(212)
-    plt.plot(D2_ets_ctrl['0'], label='D2 ctrl 0', color='k')
-    plt.plot(D2_ets_ctrl['30'], label='D2 ctrl 30', color='k', linestyle='--')
-    plt.plot(D2_ets_ctrl['60'], label='D2 ctrl 60', color='k', linestyle=':')
-    plt.plot(D2_ets_amph['0'], label='D2 amph 0', color='r')
-    plt.plot(D2_ets_amph['30'], label='D2 amph 30', color='r', linestyle='--')
-    plt.plot(D2_ets_amph['60'], label='D2 amph 60', color='r', linestyle=':')
-    x_default = [0, 1, 2, 3];
-    x_new = ['<1', '1-5', '5-10', '>10'];
+    plt.plot(D2_ets_ctrl_mean, label='D2 ctrl', color='k')
+    plt.fill_between(x, D1_ets_amph_mean + D1_ets_amph_sem, D1_ets_amph_mean - D1_ets_amph_sem, color='k', alpha=0.2)
+    plt.plot(D2_ets_amph_mean, label='D2 amph', color='r')
+    plt.fill_between(x, D1_ets_amph_mean + D1_ets_amph_sem, D1_ets_amph_mean - D1_ets_amph_sem, color='b', alpha=0.2)
+    x_default = [0, 1, 2, 3, 4];
+    x_new = ['right 60', 'right 30', 'straight 0', 'left 30', 'left 60'];
     plt.xticks(x_default, x_new);
     plt.ylim((0, 2.5))
-    plt.xlabel('Locomotor speed bin (cm/s)')
+    # plt.xlabel('Locomotor speed bin (cm/s)')
     plt.ylabel('Ca event rate (event/min)')
     plt.title('D2 SPNs')
     plt.legend()
