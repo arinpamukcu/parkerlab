476c05f7ee35fc1506089ae8d6aca05ccee94b82
Arin <arinpamukcu@gmail.com>
2022-11-02 17:33:41 -0500

    update

--- alldata.py
+++ alldata.py
@@ -16,20 +16,22 @@ def get_speed(speed_data, eventmean_data):
     speed_dt = speed_data[1:] - speed_data[:-1]
 
     for fr in range(0, len(speed_data) - 2):
-        # if np.mean(speed_dt[fr:fr + 2]) > 0:  #06
-        # if speed_data[fr - 2] < speed_data[fr - 1] < speed_data[fr]:  # 05
-        # if speed_dt[fr - 2] < speed_dt[fr - 1] < speed_dt[fr]:  # 04
+        # if np.mean(speed_dt[fr:fr + 2]) > 0:  #07
+        # if speed_data[fr - 2] < speed_data[fr - 1] < speed_data[fr]:  # 06
+        # if speed_dt[fr - 2] < speed_dt[fr - 1] < speed_dt[fr]:  # 05
+        # if np.mean(speed_data[fr - 2:fr]) <= 0.5 and np.mean(speed_dt[fr:fr + 5]) > 0:  # 04
         # if np.mean(speed_data[fr - 2:fr]) <= 0.5 and np.mean(speed_dt[fr:fr + 5]) > 0:  # 03
-        if np.mean(speed_dt[fr:fr + 2]) > 0:  # 02
-        # if speed_dt[fr] > 0:  # 01
+        # if np.mean(speed_dt[fr:fr + 2]) > 0:  # 02
+        if speed_dt[fr] > 0.25:  # 01
             acc_events.append(eventmean_data[fr])
             acc_duration += 1
-        # if np.mean(speed_dt[fr:fr + 2]) < 0:  # 06
-        # if speed_data[fr - 2] > speed_data[fr - 1] > speed_data[fr]:  # 05
-        # if speed_dt[fr - 2] > speed_dt[fr - 1] > speed_dt[fr]:  # 04
+        # if np.mean(speed_dt[fr:fr + 2]) < 0:  # 07
+        # if speed_data[fr - 2] > speed_data[fr - 1] > speed_data[fr]:  # 06
+        # if speed_dt[fr - 2] > speed_dt[fr - 1] > speed_dt[fr]:  # 05
+        # if 0.5 < np.mean(speed_data[fr - 2:fr]) and np.mean(speed_dt[fr - 2:fr]) < 0:  # 04
         # if np.mean(speed_data[fr:fr + 2]) <= 0.5 and np.mean(speed_dt[fr - 2:fr]) < 0:  # 03
-        if np.mean(speed_dt[fr:fr + 2]) < 0:  # 02
-        # if speed_dt[fr] < 0:  # 01
+        # if np.mean(speed_dt[fr:fr + 2]) < 0:  # 02
+        if speed_dt[fr] < -0.25:  # 01
             dec_events.append(eventmean_data[fr])
             dec_duration += 1
         if speed_data[fr] <= 0.5:
@@ -56,7 +58,7 @@ def get_turns(turn_data, eventmean_data):
     # TRY just right vs left turn, here or when plotting
 
     for fr in range(2, len(turn_data) - 2):
-        if np.mean(turn_dt[fr - 2:fr + 2]) >= 30:  # turn right
+        if np.mean(turn_dt[fr - 1:fr + 1]) >= 30:  # turn right
             right_events.append(eventmean_data[fr])
             right_duration += 1
         # if turn_data[fr] <= -10 and np.mean(turn_dt[fr-2:fr+2]) >= 30:  # turn right, from left to center # TRY higher ang vel
@@ -71,7 +73,7 @@ def get_turns(turn_data, eventmean_data):
         # if turn_data[fr] >= 10 and np.mean(turn_dt[fr-2:fr+2]) <= -30:  # turn left, from right to center
         #     righttocenter_events.append(eventmean_data[fr])
         #     righttocenter_duration += 1
-        if np.mean(turn_dt[fr - 2:fr + 2]) <= -30:  # turn left
+        if np.mean(turn_dt[fr - 1:fr + 1]) <= -30:  # turn left
             left_events.append(eventmean_data[fr])
             left_duration += 1
 
@@ -106,6 +108,9 @@ def get_metrics(drug, dose):
         right_turn_amph, left_turn_amph = get_turns(turn_angle_amph, eventmean_amph)
 
         # append values for each animal to a list
+        data_ctrl[animal]['events'] = eventmean_ctrl
+        data_ctrl[animal]['speed'] = speed_ctrl
+        data_ctrl[animal]['turn'] = turn_angle_ctrl
         data_ctrl[animal]['acc'] = acc_ctrl
         data_ctrl[animal]['dec'] = dec_ctrl
         data_ctrl[animal]['rest'] = rest_ctrl
@@ -113,6 +118,9 @@ def get_metrics(drug, dose):
         data_ctrl[animal]['right_turn'] = right_turn_ctrl
         data_ctrl[animal]['left_turn'] = left_turn_ctrl
 
+        data_amph[animal]['events'] = eventmean_amph
+        data_amph[animal]['speed'] = speed_amph
+        data_amph[animal]['turn'] = turn_angle_amph
         data_amph[animal]['acc'] = acc_amph
         data_amph[animal]['dec'] = dec_amph
         data_amph[animal]['rest'] = rest_amph
@@ -126,7 +134,8 @@ def get_metrics(drug, dose):
 def get_alldata():
 
     drugs = ['clozapine', 'haloperidol', 'mp10', 'olanzapine']
-    doses = ['vehicle', 'lowdose', 'highdose']
+    # doses = ['vehicle', 'lowdose', 'highdose']
+    doses = ['vehicle', 'highdose']
 
     alldata = {}
     for drug in drugs:
@@ -149,111 +158,9 @@ def get_alldata():
     return alldata
 
 
-def plot_timespent(drug, dose):
-
-    bases = ['ctrl', 'amph']
-    metrics = ['acc', 'dec', 'rest', 'move', 'right_turn', 'left_turn']
-
-    alldata = pkl.load(open("alldata.pkl", "rb"))
-    # alldata = get_alldata()
-
-    # for dose in doses:
-    if dose == 'vehicle':
-        plt.figure(figsize=(8, 5))
-        x = 0
-        for metric in metrics:
-            a = 1
-            x = x + 1
-            for base in bases:
-                data = [alldata[d][dose][base][a][metric] for d in alldata.keys()
-                        for a in alldata[d][dose][base].keys()]
-                mean = np.nanmean(data, axis=0)
-                sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
-                plt.bar(x, mean[0], yerr=sem[0], width=1, color='k', alpha=a)
-                a = a - 0.5
-                x = x + 1
-    else:
-        plt.figure(figsize=(8, 5))
-        x = 0
-        for metric in metrics:
-            a = 1
-            x = x + 1
-            for base in bases:
-                data = [alldata[drug][dose][base][a][metric] for a in alldata[drug][dose][base].keys()]
-                mean = np.nanmean(data, axis=0)
-                sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
-                plt.bar(x, mean[0], yerr=sem[0], width=1, color='k', alpha=a)
-                a = a - 0.5
-                x = x + 1
-
-    # plt.suptitle(' '.join(drug) + ', ' + dose)
-    x_default = [1.5, 4.5, 7.5, 10.5, 13.5, 16.5]
-    plt.xticks(x_default, metrics)
-    # plt.xticks(rotation=20, fontsize=8)
-    plt.ylabel('time (s)')
-    plt.ylim((0, 1))
-    plt.legend(['ctrl', 'amph'])
-    plt.show()
-
-    return
-
-
-def plot_eventrate(drug, dose):
-
-    bases = ['ctrl', 'amph']
-    metrics = ['acc', 'dec', 'rest', 'move', 'right_turn', 'left_turn']
-
-    D1_animals, D2_animals = D1_D2_names()
-
-    alldata = pkl.load(open("alldata.pkl", "rb"))
-    # alldata = get_alldata()
-
-    # for drug in drugs:
-    if dose == 'vehicle':
-        plt.figure(figsize=(8, 5))
-        x = 0
-        for metric in metrics:
-            a = 1
-            x = x + 1
-            for base in bases:
-                data = [alldata[d][dose][base][a][metric] for d in
-                     alldata.keys() for a in alldata[d][dose][base].keys() if a in D2_animals]
-                mean = np.ma.masked_invalid(data).mean(axis=0)
-                sem = np.ma.masked_invalid(data).std(axis=0) / np.sqrt(len(data))
-                plt.bar(x, mean[1], yerr=sem[1], width=1, color='k', alpha=a)
-                a = a - 0.5
-                x = x + 1
-
-    else:
-        plt.figure(figsize=(8, 5))
-        x = 0
-        for metric in metrics:
-            a = 1
-            x = x + 1
-            for base in bases:
-                data = [alldata[drug][dose][base][a][metric] for a in alldata[drug][dose][base].keys() if
-                     a in D2_animals]
-                mean = np.ma.masked_invalid(data).mean(axis=0)
-                sem = np.ma.masked_invalid(data).std(axis=0) / np.sqrt(len(data))
-                plt.bar(x, mean[1], yerr=sem[1], width=1, color='k', alpha=a)
-                a = a - 0.5
-                x = x + 1
-
-    plt.suptitle(' '.join(drug) + ', ' + base)
-    # plt.title('n = ' + str(len(y)))
-    # plt.xticks(rotation=20, fontsize=8)
-    x_default = [1.5, 4.5, 7.5, 10.5, 13.5, 16.5]
-    plt.xticks(x_default, metrics)
-    plt.ylabel('event rate (event/s)')
-    plt.ylim((0, 0.07))
-    plt.show()
-
-    return
-
-
 def plot_times():
 
-    drugs = ['haloperidol', 'clozapine', 'olanzapine', 'mp10']
+    drugs = ['haloperidol', 'olanzapine', 'clozapine', 'mp10']
     doses = ['vehicle', 'highdose']
     bases = ['ctrl', 'amph']
     metrics = ['acc', 'dec', 'rest', 'move', 'right_turn', 'left_turn']
@@ -272,27 +179,23 @@ def plot_times():
                             for a in alldata[d][dose][base].keys()]
                     mean = np.nanmean(data, axis=0)
                     sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
-                    print(base)
-                    print(mean[0])
                     ax.bar(x, mean[0], yerr=sem[0], width=1, color=color)
                     x = x + 1
-            else:
-                colors = ['royalblue', 'orangered', 'forestgreen', 'darkviolet']
-                for drug, color in zip(drugs, colors):
-                    x = x + 0.5
-                    data = [alldata[drug][dose]['amph'][a][metric] for a in alldata[drug][dose]['amph'].keys()]
-                    mean = np.nanmean(data, axis=0)
-                    sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
-                    print(drug)
-                    print(mean[0])
-                    ax.bar(x, mean[0], yerr=sem[0], width=1, color=color, alpha=0.7)
-                    x = x + 1
+            # else:
+            #     colors = ['royalblue', 'orangered', 'forestgreen', 'darkviolet']
+            #     for drug, color in zip(drugs, colors):
+            #         x = x + 0.5
+            #         data = [alldata[drug][dose]['amph'][a][metric] for a in alldata[drug][dose]['amph'].keys()]
+            #         mean = np.nanmean(data, axis=0)
+            #         sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
+            #         ax.bar(x, mean[0], yerr=sem[0], width=1, color=color, alpha=0.7)
+            #         x = x + 1
 
     plt.suptitle('fraction of time spent performing behaviors')
-    plt.legend(['ctrl', 'amph', 'haloperidol', 'clozapine', 'olanzapine', 'mp10'])
-    x_default = [2.5, 12.5, 22.5, 32.5, 42.5, 52.5]
-    # plt.legend(['ctrl', 'amph'])
-    # x_default = [2.5, 6.5, 10.5, 14.5, 18.5, 22.5]
+    # plt.legend(['ctrl', 'amph', 'haloperidol', 'olanzapine', 'clozapine', 'mp10'])
+    # x_default = [2.5, 12.5, 22.5, 32.5, 42.5, 52.5]
+    plt.legend(['ctrl', 'amph'])
+    x_default = [2.5, 6.5, 10.5, 14.5, 18.5, 22.5]
     plt.xticks(x_default, metrics)
     plt.xticks(fontsize=8)
     plt.ylabel('time spent fraction')
@@ -313,9 +216,9 @@ def plot_events(spn):
     D1_animals, D2_animals = D1_D2_names()
 
     animals = []
-    if spn == 'D1':
+    if spn == 'd1':
         animals = D1_animals
-    elif spn == 'D2':
+    elif spn == 'd2':
         animals = D2_animals
 
     # plt.figure(figsize=(8, 5))
@@ -327,31 +230,65 @@ def plot_events(spn):
                 colors = ['black', 'gray']
                 x = x + 2
                 for base, color in zip(bases, colors):
-                    data = [alldata[d][dose][base][s][metric] for d in
-                            alldata.keys() for s in alldata[d][dose][base].keys() if s in animals]
+                    data = [alldata[d][dose][base][s][metric] for d in alldata.keys()
+                            for s in alldata[d][dose][base].keys() if s in animals]
                     mean = np.nanmean(data, axis=0)
                     sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
                     ax.bar(x, mean[1], yerr=sem[1], width=1, color=color)
                     x = x + 1
-            else:
-                colors = ['royalblue', 'orangered', 'forestgreen', 'darkviolet']
-                for drug, color in zip(drugs, colors):
-                    x = x + 0.5
-                    data = [alldata[drug][dose]['amph'][s][metric] for s in alldata[drug][dose]['amph'].keys() if
-                            s in animals]
-                    mean = np.nanmean(data, axis=0)
-                    sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
-                    ax.bar(x, mean[1], yerr=sem[1], width=1, color=color, alpha=0.7)
-                    x = x + 1
+            # else:
+            #     colors = ['royalblue', 'orangered', 'forestgreen', 'darkviolet']
+            #     for drug, color in zip(drugs, colors):
+            #         x = x + 0.5
+            #         data = [alldata[drug][dose]['amph'][s][metric] for s in alldata[drug][dose]['amph'].keys() if
+            #                 s in animals]
+            #         mean = np.nanmean(data, axis=0)
+            #         sem = np.nanstd(data, axis=0) / np.sqrt(len(data))
+            #         ax.bar(x, mean[1], yerr=sem[1], width=1, color=color, alpha=0.7)
+            #         x = x + 1
 
     plt.suptitle(str(spn) + ' SPN event rate during performing behaviors')
-    # plt.title('n = ' + str(len(data)))
-    x_default = [2.5, 12.5, 22.5, 32.5, 42.5, 52.5]
+    # plt.legend(['ctrl', 'amph', 'haloperidol', 'clozapine', 'olanzapine', 'mp10'])
+    # x_default = [2.5, 12.5, 22.5, 32.5, 42.5, 52.5]
+    plt.legend(['ctrl', 'amph'])
+    x_default = [2.5, 6.5, 10.5, 14.5, 18.5, 22.5]
     plt.xticks(x_default, metrics)
     plt.xticks(fontsize=8)
     plt.ylabel('event rate (event/s)')
     plt.ylim((0, 0.07))
-    plt.legend(['ctrl', 'amph', 'haloperidol', 'clozapine', 'olanzapine', 'mp10'])
     plt.show()
 
     return
+
+
+def plot_histogram(metric):
+    # drugs = ['haloperidol', 'olanzapine', 'clozapine', 'mp10']
+    # doses = ['vehicle', 'highdose']
+    bases = ['ctrl', 'amph']
+    # metrics = ['speed']
+
+    alldata = pkl.load(open("alldata.pkl", "rb"))
+
+    fig, ax = plt.subplots(2, 2, figsize=(8, 7))
+    for i, base in enumerate(bases):
+        data = [alldata[d]['vehicle'][base][a][metric] for d in alldata.keys() for a in alldata[d]['vehicle'][base].keys()]
+        data = np.mean(np.array(data), axis=0)
+        data_dt = data[1:] - data[:-1]
+
+        # plot histogram
+        ax[0, i].hist(data_dt, bins=20)
+        ax[0, i].set_title(base + ' historagam')
+
+        count, bins_count = np.histogram(data_dt, bins=50)
+        pdf = count / sum(count)  # find the PDF of the histogram using count values
+        cdf = np.cumsum(pdf)  # use numpy cumsum to calculate the CDF
+
+        ax[1, i].plot(bins_count[1:], pdf, color='r', label='PDF')
+        ax[1, i].plot(bins_count[1:], cdf, label='CDF')
+        ax[1, i].set_title(base + ' pdf/cdf')
+
+    plt.legend()
+    plt.show()
+
+    return
+
