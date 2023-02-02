f545e7fe758422f525739bea5f359cf258ab15a0
Arin <arinpamukcu@gmail.com>
2023-02-02 17:15:48 -0600

    glm update

--- glm_neurons.py
+++ glm_neurons.py
@@ -41,56 +41,96 @@ def prep_data(drug, dose, shift):
     alldata = pkl.load(open("alldata.pkl", "rb"))
     D1_animals, D2_animals = D1_D2_names()
 
-    d1_network_ctrl, d1_network_amph, d1_speed_ctrl, d1_speed_amph = ([] for i in range(4))
-    d2_network_ctrl, d2_network_amph, d2_speed_ctrl, d2_speed_amph = ([] for i in range(4))
+    d1_network_ctrl, d1_network_amph, d1_speed_ctrl, d1_speed_amph, d1_turn_ctrl, d1_turn_amph, \
+    d1_groom_ctrl, d1_groom_amph, d1_rear_ctrl, d1_rear_amph = ([] for i in range(10))
+    d2_network_ctrl, d2_network_amph, d2_speed_ctrl, d2_speed_amph, d2_turn_ctrl, d2_turn_amph, \
+    d2_groom_ctrl, d2_groom_amph, d2_rear_ctrl, d2_rear_amph = ([] for i in range(10))
 
     for experiment, animal in zip(experiments, animals):
-        print(experiment)
+        # print(experiment)
 
         _, _, calcium_ctrl_processed, calcium_amph_processed, \
         _, _, _, = get_calcium_dff(drug, dose, experiment)
         speed_ctrl, speed_amph, events_ctrl, events_amph, eventmean_ctrl, eventmean_amph, \
         _, _, _ = get_calcium_events(drug, dose, experiment)
 
-        turn_ctrl, turn_amph \
-            = get_mars_features(drug, dose, experiment)
-        grooming_ctrl, grooming_amph \
-            = get_classifier(drug, dose, experiment, 'grooming')
-        rearing_ctrl, rearing_amph \
-            = get_classifier(drug, dose, experiment, 'rearing')
+        turn_ctrl, turn_amph = get_mars_features(drug, dose, experiment)
+        groom_ctrl, groom_amph = get_classifier(drug, dose, experiment, 'grooming')
+        rear_ctrl, rear_amph = get_classifier(drug, dose, experiment, 'rearing')
 
         if animal in alldata[drug][dose]['ctrl'].keys():
             if animal in D1_animals:
+                print('D1_animal: ' + experiment)
 
                 d1_network_ctrl = np.hstack((d1_network_ctrl, eventmean_ctrl[:-shift]))
                 d1_network_amph = np.hstack((d1_network_amph, eventmean_amph[:-shift]))
 
                 speed_ctrl_shifted = feature_future(speed_ctrl[:], shift).T
                 speed_amph_shifted = feature_future(speed_amph[:], shift).T
+                turn_ctrl_shifted = feature_future(turn_ctrl[:], shift).T
+                turn_amph_shifted = feature_future(turn_amph[:], shift).T
+                groom_ctrl_shifted = feature_future(groom_ctrl[:], shift).T
+                groom_amph_shifted = feature_future(groom_amph[:], shift).T
+                rear_ctrl_shifted = feature_future(rear_ctrl[:], shift).T
+                rear_amph_shifted = feature_future(rear_amph[:], shift).T
 
                 d1_speed_ctrl.extend(speed_ctrl_shifted)
                 d1_speed_amph.extend(speed_amph_shifted)
+                d1_turn_ctrl.extend(turn_ctrl_shifted)
+                d1_turn_amph.extend(turn_amph_shifted)
+                d1_groom_ctrl.extend(groom_ctrl_shifted)
+                d1_groom_amph.extend(groom_amph_shifted)
+                d1_rear_ctrl.extend(rear_ctrl_shifted)
+                d1_rear_amph.extend(rear_amph_shifted)
 
             elif animal in D2_animals:
+                print('D2_animal: ' + experiment)
                 d2_network_ctrl = np.hstack((d2_network_ctrl, eventmean_ctrl[:-shift]))
                 d2_network_amph = np.hstack((d2_network_amph, eventmean_amph[:-shift]))
 
                 speed_ctrl_shifted = feature_future(speed_ctrl[:], shift).T
                 speed_amph_shifted = feature_future(speed_amph[:], shift).T
+                turn_ctrl_shifted = feature_future(turn_ctrl[:], shift).T
+                turn_amph_shifted = feature_future(turn_amph[:], shift).T
+                groom_ctrl_shifted = feature_future(groom_ctrl[:], shift).T
+                groom_amph_shifted = feature_future(groom_amph[:], shift).T
+                rear_ctrl_shifted = feature_future(rear_ctrl[:], shift).T
+                rear_amph_shifted = feature_future(rear_amph[:], shift).T
 
                 d2_speed_ctrl.extend(speed_ctrl_shifted)
                 d2_speed_amph.extend(speed_amph_shifted)
+                d2_turn_ctrl.extend(turn_ctrl_shifted)
+                d2_turn_amph.extend(turn_amph_shifted)
+                d2_groom_ctrl.extend(groom_ctrl_shifted)
+                d2_groom_amph.extend(groom_amph_shifted)
+                d2_rear_ctrl.extend(rear_ctrl_shifted)
+                d2_rear_amph.extend(rear_amph_shifted)
 
     # add 10 seconds of feature future
-    d1_speed_ctrl_shifted = np.array(d1_speed_ctrl)
-    d1_speed_amph_shifted = np.array(d1_speed_amph)
-    d2_speed_ctrl_shifted = np.array(d2_speed_ctrl)
-    d2_speed_amph_shifted = np.array(d2_speed_amph)
+    d1_speed_ctrl_shifted = np.array(d1_speed_ctrl).T
+    d1_speed_amph_shifted = np.array(d1_speed_amph).T
+    d1_turn_ctrl_shifted = np.array(d1_turn_ctrl).T
+    d1_turn_amph_shifted = np.array(d1_turn_amph).T
+    d1_groom_ctrl_shifted = np.array(d1_groom_ctrl).T
+    d1_groom_amph_shifted = np.array(d1_groom_amph).T
+    d1_rear_ctrl_shifted = np.array(d1_rear_ctrl).T
+    d1_rear_amph_shifted = np.array(d1_rear_amph).T
 
-    d1_ctrl_regressor = np.vstack((d1_speed_ctrl_shifted.T, d1_network_ctrl.T))
-    d1_amph_regressor = np.vstack((d1_speed_amph_shifted.T, d1_network_amph.T))
-    d2_ctrl_regressor = np.vstack((d2_speed_ctrl_shifted.T, d2_network_ctrl.T))
-    d2_amph_regressor = np.vstack((d2_speed_amph_shifted.T, d2_network_amph.T))
+    d2_speed_ctrl_shifted = np.array(d2_speed_ctrl).T
+    d2_speed_amph_shifted = np.array(d2_speed_amph).T
+    d2_turn_ctrl_shifted = np.array(d2_turn_ctrl).T
+    d2_turn_amph_shifted = np.array(d2_turn_amph).T
+    d2_groom_ctrl_shifted = np.array(d2_groom_ctrl).T
+    d2_groom_amph_shifted = np.array(d2_groom_amph).T
+    d2_rear_ctrl_shifted = np.array(d2_rear_ctrl).T
+    d2_rear_amph_shifted = np.array(d2_rear_amph).T
+
+    pdb.set_trace()
+
+    d1_ctrl_regressor = np.concatenate((d1_speed_ctrl_shifted, d1_turn_ctrl_shifted, d1_groom_ctrl_shifted, d1_rear_ctrl_shifted, d1_network_ctrl))
+    d1_amph_regressor = np.concatenate((d1_speed_amph_shifted, d1_turn_amph_shifted, d1_groom_amph_shifted, d1_rear_amph_shifted, d1_network_amph))
+    d2_ctrl_regressor = np.concatenate((d2_speed_ctrl_shifted, d2_turn_ctrl_shifted, d2_groom_ctrl_shifted, d2_rear_ctrl_shifted, d2_network_ctrl))
+    d2_amph_regressor = np.concatenate((d2_speed_amph_shifted, d2_turn_amph_shifted, d2_groom_amph_shifted, d2_rear_amph_shifted, d2_network_amph))
 
     return d1_ctrl_regressor, d1_amph_regressor, d2_ctrl_regressor, d2_amph_regressor
 
